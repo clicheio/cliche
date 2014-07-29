0470802a0bde93c5b4c8b051b374a5990a95242a
Hong Minhee <minhee@dahlia.kr>
2014-07-30 00:04:46 +0900

    Add Team class

--- cliche/people.py
+++ cliche/people.py
@@ -7,7 +7,7 @@ from sqlalchemy.types import Date, DateTime, Integer, String
 
 from .orm import Base
 
-__all__ = {'Person'}
+__all__ = {'Person', 'Team'}
 
 
 class Person(Base):
@@ -26,4 +26,20 @@ class Person(Base):
     created_at = Column(DateTime(timezone=True), nullable=False, index=True)
 
     __tablename__ = 'people'
+    __repr_columns__ = id, name
+
+
+class Team(Base):
+    """Teams (including ad-hoc teams)."""
+
+    #: (:class:`int`) The primary key integer.
+    id = Column(Integer, primary_key=True)
+
+    #: (:class:`str`) The team name (if it's named).
+    name = Column(String, index=True)
+
+    #: (:class:`datetime.datetime`) The created time.
+    created_at = Column(DateTime(timezone=True), nullable=False, index=True)
+
+    __tablename__ = 'teams'
     __repr_columns__ = id, name
