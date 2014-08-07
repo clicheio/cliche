86a3c0eb11682e205821d3a8289bc9748e1a129e
Tki April <tki@tki.so>
2014-08-08 00:36:09 +0900

    TVTropes crawler use sqlalchemy

--- cliche/services/tvtropes/crawler.py
+++ cliche/services/tvtropes/crawler.py
@@ -5,8 +5,11 @@ import urllib.parse
 
 from celery.utils.log import get_task_logger
 from lxml.html import parse
-import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
 
+from .entities import Entity, Relation
+from ...orm import Base, Session
 from ...worker import worker
 
 
@@ -48,27 +51,32 @@ def list_pages(namespace_url=None):
 
 
 @worker.task
-def save_link(name, url):
-    with psycopg2.connect(worker.conf.DATABASE_URL) as conn:
-        cur = conn.cursor
-        try:
-            type = determine_type(name[0])
-            cur.execute('INSERT INTO entities VALUES (%s, %s, %s, NULL, %s)',
-                        (name[0], name[1], url, type))
-        except psycopg2.IntegrityError:
-            cur.execute('UPDATE entities SET url = %s '
-                        'WHERE namespace = %s and name = %s',
-                        (url, name[0], name[1]))
-            conn.rollback()
-        conn.commit()
-        cur.execute('SELECT count(*) FROM entities')
-        get_task_logger(__name__ + '.save_link').info(
-            'Total %d',
-            cur.fetchone()[0]
-        )
+def save_link(namepair, url):
+    namespace, name = namepair
+    engine = create_engine(worker.conf.DATABASE_URL)
+    session = Session(bind=engine)
+    try:
+        with session.begin():
+            new_entity = Entity(
+                namespace=namespace,
+                name=name,
+                url=url,
+                type=determine_type(namespace)
+            )
+            session.add(new_entity)
+    except IntegrityError:
+        with session.begin():
+            entity = session.query(Entity) \
+                            .filter_by(namespace=namespace, name=name) \
+                            .one()
+            entity.url = url
+    get_task_logger(__name__ + '.save_link').info(
+        'Total %d',
+        session.query(Entity).count()
+    )
 
 
-def fetch_link(url, conn, cur):
+def fetch_link(url, session):
     tree = parse(url)
     try:
         namespace = tree.xpath('//div[@class="pagetitle"]')[0] \
@@ -78,13 +86,14 @@ def fetch_link(url, conn, cur):
     if namespace == '':
         namespace = 'Main'
     name = tree.xpath('//div[@class="pagetitle"]/span')[0].text.strip()
-    try:
-        type = determine_type(namespace)
-        cur.execute('INSERT INTO entities VALUES (%s, %s, %s, NULL, %s)',
-                    (namespace, name, url, type))
-    except psycopg2.IntegrityError:
-        conn.rollback()
-    conn.commit()
+    with session.begin():
+        new_entity = Entity(
+            namespace=namespace,
+            name=name,
+            url=url,
+            type=determine_type(namespace)
+        )
+        session.add(new_entity)
     return tree, namespace, name
 
 
@@ -92,120 +101,92 @@ def fetch_link(url, conn, cur):
 def crawl_link(url, start_time,
                start_indexindex_count, start_relations_count,
                round_count):
-    with psycopg2.connect(worker.conf.DATABASE_URL) as conn:
-        cur = conn.cursor()
-        logger = get_task_logger(__name__ + '.crawl_link')
-        current_time = datetime.now()
+    engine = create_engine(worker.conf.DATABASE_URL)
+    session = Session(bind=engine)
+    logger = get_task_logger(__name__ + '.crawl_link')
+    current_time = datetime.now()
 
-        cur.execute('SELECT last_crawled FROM entities '
-                    'WHERE url = %s',
-                    (url,))
-        last_crawled = cur.fetchone()
-        if last_crawled and last_crawled[0]:
-            if (current_time - last_crawled[0]) < CRAWL_INTERVAL:
-                logger.info('Skipping: {} due to '
-                            'recent crawl in {} days'
-                            .format(url, CRAWL_INTERVAL))
-                conn.close()
-                return
-
-        fetch_result = fetch_link(url, conn, cur)
-        if fetch_result is None:
-            logger.warning('Warning: There is no pagetitle on this page. '
-                           'Ignoring.')
+    last_crawled = session.query(Entity).filter_by(url=url) \
+                          .first().last_crawled
+    if last_crawled:
+        if current_time - last_crawled < CRAWL_INTERVAL:
+            logger.info('Skipping: {} due to '
+                        'recent crawl in {} days'
+                        .format(url, CRAWL_INTERVAL))
             return
-        tree, namespace, name = fetch_result
-        logger.info("Fetching: {}/{} @ {}"
-                    .format(namespace, name, url))
-        for a in tree.xpath('//a[@class="twikilink"]'):
-            try:
-                destination_url = urllib.parse.urljoin(
-                    WIKI_PAGE, a.attrib['href']
+
+    fetch_result = fetch_link(url, session)
+    if fetch_result is None:
+        logger.warning('Warning: There is no pagetitle on this page. '
+                       'Ignoring.')
+        return
+    tree, namespace, name = fetch_result
+    logger.info("Fetching: {}/{} @ {}"
+                .format(namespace, name, url))
+    for a in tree.xpath('//a[@class="twikilink"]'):
+        try:
+            destination_url = urllib.parse.urljoin(
+                WIKI_PAGE, a.attrib['href']
+            )
+            fetch_result = fetch_link(url, session)
+            if fetch_result is None:
+                logger.warning('Warning: There is no pagetitle on '
+                               'this page. Ignoring.')
+                return
+            destination_tree, destination_namespace, \
+                destination_name = fetch_result
+            with session.begin():
+                new_relation = Relation(
+                    origin_namespace=namespace,
+                    origin_name=name,
+                    destination_namespace=destination_namespace,
+                    destination_name=destination_name
                 )
-                fetch_result = fetch_link(url, conn, cur)
-                if fetch_result is None:
-                    logger.warning('Warning: There is no pagetitle on '
-                                   'this page. Ignoring.')
-                    return
-                destination_tree, destination_namespace, \
-                    destination_name = fetch_result
-                try:
-                    cur.execute('INSERT INTO relations VALUES '
-                                '(%s, %s, %s, %s)',
-                                (namespace, name,
-                                 destination_namespace,
-                                 destination_name))
-                except psycopg2.IntegrityError:
-                    conn.rollback()
-                # FIXME if next_crawl not in crawl_stack:
-                crawl_link.delay(destination_url, start_time,
-                                 start_indexindex_count, start_relations_count,
-                                 round_count)
-            except AttributeError:
-                pass
-        logger.info('Crawling {}/{} @ {} completed at {}'
-                    .format(namespace, name, url, current_time))
-        cur.execute('''
-            UPDATE entities SET last_crawled = %s
-            WHERE namespace = %s and name = %s
-                ''', (current_time, namespace, name))
-        round_count += 1
-        if round_count >= 10:
-            round_count = 0
-            elasped = datetime.now() - start_time
-            elasped_hours = elasped.total_seconds() / 3600
-            cur.execute('SELECT count(*) FROM entities')
-            indexindex_count = int(cur.fetchone()[0]) - start_indexindex_count
-            cur.execute('SELECT count(*) FROM relations')
-            relations_count = int(cur.fetchone()[0]) - start_relations_count
-            logger.info('-> entities: %s (%s/h) relations: %s (%s/h) '
-                        'elasped %s',
-                        indexindex_count,
-                        int(indexindex_count / elasped_hours),
-                        relations_count,
-                        int(relations_count / elasped_hours),
-                        elasped)
+                session.add(new_relation)
+            # FIXME if next_crawl not in crawl_stack:
+            crawl_link.delay(destination_url, start_time,
+                             start_indexindex_count, start_relations_count,
+                             round_count)
+        except AttributeError:
+            pass
+    logger.info('Crawling {}/{} @ {} completed at {}'
+                .format(namespace, name, url, current_time))
+    with session.begin():
+        entity = session.query(Entity) \
+                        .filter_by(url=url) \
+                        .one()
+        entity.last_crawled = current_time
+    round_count += 1
+    if round_count >= 10:
+        round_count = 0
+        elasped = datetime.now() - start_time
+        elasped_hours = elasped.total_seconds() / 3600
+        indexindex_count = session.query(Entity).count() \
+            - start_indexindex_count
+        relations_count = session.query(Relation).count() \
+            - start_relations_count
+        logger.info('-> entities: %s (%s/h) relations: %s (%s/h) '
+                    'elasped %s',
+                    indexindex_count,
+                    int(indexindex_count / elasped_hours),
+                    relations_count,
+                    int(relations_count / elasped_hours),
+                    elasped)
 
 
 def crawl(config):
     worker.config_from_object(config)
-    with psycopg2.connect(worker.conf.DATABASE_URL) as conn:
-        cur = conn.cursor()
-        cur.execute('''
-            CREATE TABLE IF NOT EXISTS entities
-            (
-            namespace text, name text, url text, last_crawled timestamp,
-            type text,
-            constraint pk_entities primary key(namespace, name)
-            )
-        ''')
-        conn.commit()
-        cur.execute('SELECT count(*) FROM entities')
-        if cur.fetchone()[0] < 1:
-            for name, url in list_pages():
-                save_link.delay(name, url)
+    engine = create_engine(worker.conf.DATABASE_URL)
+    session = Session(bind=engine)
 
-        cur.execute('''
-            CREATE TABLE IF NOT EXISTS relations
-            (
-            origin_namespace text, origin text,
-            destination_namespace text, destination text,
-            constraint pk_relations primary key(origin_namespace, origin,
-            destination_namespace, destination)
-            )
-            ''')
-        conn.commit()
-        cur.execute('SELECT namespace, name, url FROM entities '
-                    'ORDER BY namespace asc, name asc')
-        seed = cur.fetchall()
-        start_time = datetime.now()
-        cur.execute('SELECT count(*) FROM entities')
-        start_indexindex_count = int(cur.fetchone()[0])
-        cur.execute('SELECT count(*) FROM relations')
-        start_relations_count = int(cur.fetchone()[0])
-        round_count = 0
-        for namespace, name, url in seed:
-            crawl_link.delay(url, start_time,
-                             start_indexindex_count, start_relations_count,
-                             round_count)
-        conn.commit()
+    Base.metadata.create_all(engine)
+    if session.query(Entity).count() < 1:
+        for name, url in list_pages():
+            save_link.delay(name, url)
+
+    for entity in session.query(Entity) \
+                         .order_by(Entity.namespace, Entity.name):
+        crawl_link.delay(entity.url, datetime.now(),
+                         session.query(Entity).count(),
+                         session.query(Relation).count(),
+                         0)
