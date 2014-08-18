b907a92031288a6fc5e22bdd1db086be9aff4e7a
Tki April <tki@tki.so>
2014-08-18 22:49:34 +0900

    Add alembic migration

---
+++ cliche/migrations/versions/3ae4102055a_clarify_column_names_in_relation.py
@@ -0,0 +1,28 @@
+"""Clarify column names in Relation
+
+Revision ID: 3ae4102055a
+Revises: 2fbf050c31f
+Create Date: 2014-08-18 22:30:58.780203
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3ae4102055a'
+down_revision = '2fbf050c31f'
+
+
+def upgrade():
+    op.alter_column('tvtropes_relations', 'origin',
+                new_column_name='origin_name')
+    op.alter_column('tvtropes_relations', 'destination',
+                new_column_name='destination_name')
+
+
+def downgrade():
+    op.alter_column('tvtropes_relations', 'destination_name',
+                new_column_name='destination')
+    op.alter_column('tvtropes_relations', 'origin_name',
+                new_column_name='origin')
