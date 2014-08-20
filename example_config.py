23c4f702008ebc5c49ca1f6c7b899e1bb9672aea
Hong Minhee <minhee@dahlia.kr>
2014-08-20 22:44:07 +0900

    Reformat example config to YAML

--- example_config.py
+++
@@ -1,7 +0,0 @@
-DATABASE_URL = '<database url goes here>'
-# http://docs.celeryproject.org/en/latest/configuration.html
-BROKER_URL = 'redis://localhost/1'
-
-CELERY_IMPORTS = [
-    'cliche.crawler'
-]
