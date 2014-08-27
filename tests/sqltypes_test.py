f2012869d3e16f0a610e18021e6ec8967eddf635
Jaewon Song <sjw0687@gmail.com>
2014-08-28 02:20:05 +0900

    Add name to EnumType in test since pgsql needs it.

--- tests/sqltypes_test.py
+++ tests/sqltypes_test.py
@@ -19,18 +19,18 @@ class ColorTable(Base):
     __tablename__ = 'color_table'
 
     id = Column(Integer, primary_key=True)
-    test_col = Column(EnumType(Color))
+    color = Column(EnumType(Color, name='color'))
 
 
 def test_enum_type(fx_session):
-    red_obj = ColorTable(test_col=Color.red)
-    green_obj = ColorTable(test_col=Color.green)
-    blue_obj = ColorTable(test_col=Color.blue)
+    red_obj = ColorTable(color=Color.red)
+    green_obj = ColorTable(color=Color.green)
+    blue_obj = ColorTable(color=Color.blue)
     fx_session.add(red_obj)
     fx_session.add(green_obj)
     fx_session.add(blue_obj)
     fx_session.flush()
     result_obj = fx_session.query(ColorTable) \
-                           .filter(ColorTable.test_col == Color.green) \
+                           .filter(ColorTable.color == Color.green) \
                            .one()
     assert green_obj is result_obj
