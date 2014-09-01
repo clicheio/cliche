4260ebbcd58b57d1fc5ded1eb919d43773418389
Tki April <tki@tki.so>
2014-09-01 23:15:16 +0900

    Create whl file for deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,17 +8,21 @@ deploy() {
 }
 
 usage() {
-	me=`basename $0`
+	me=$(basename $0)
 	echo "usage: ./$me <list of ip address split by spaces>"
 	exit 1
 }
 
 main() {
 	if [[ $@ ]]; then
-		for address in "$@"; do
-
-			deploy $address
-		done
+		workdir=$(dirname $0)/..
+		cd $workdir
+		rm -rf dist/
+		revision=$(cat .git/$(awk '{print $2}' .git/HEAD))
+		python setup.py egg_info -b "-$revision" bdist_wheel
+			for address in "$@"; do
+				deploy $address
+			done
 	else
 		usage
 	fi
