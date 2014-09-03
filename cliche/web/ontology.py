41738ed90f5845ceed067b2f4253b3b402edd10f
item4_macbook_pro <item4_hun@hotmail.com>
2014-09-03 23:05:20 +0900

    change premature optimization

--- cliche/web/ontology.py
+++ cliche/web/ontology.py
@@ -6,11 +6,6 @@ from sqlalchemy.orm.exc import NoResultFound
 from .db import session
 from ..work import Work
 
-type_map = {
-    # name, ORM Table class, field for total list
-    'work': ('Work', Work, Work.name)
-}
-
 ontology = Blueprint('ontology', __name__,
                      template_folder='templates/ontology/')
 
@@ -20,40 +15,30 @@ def index():
     return 'Hello cliche!'
 
 
-@ontology.route('/<type_>/')
-def list_(type_):
-    temp = type_map.get(type_)
-    if temp:
-        type_name, type_class, type_field = temp
-        res = [
-            line[0]
-            for line in session.query(type_field).order_by(type_field)
-        ]
-        return render_template(
-            'list.html',
-            page_title='cliche.io - list of ' + type_name,
-            type=type_name,
-            list=res
-        )
-    else:
-        abort(404)
+@ontology.route('/work/')
+def list_():
+    res = [
+        line[0]
+        for line in session.query(Work.name).order_by(Work.name)
+    ]
+    return render_template(
+        'list.html',
+        page_title='cliche.io - list of Work',
+        type='Work',
+        list=res
+    )
 
 
-@ontology.route('/<type_>/<path:title>/')
-def page(type_, title):
-    temp = type_map.get(type_)
-    if temp:
-        type_name, type_class, type_field = temp
-        try:
-            res = session.query(type_class).filter(type_field == title).one()
-        except NoResultFound:
-            abort(404)
-        return render_template(
-            'page_%s.html' % type_,
-            page_title='cliche.io - contents of ' + title,
-            type=type_name,
-            title=title,
-            res=res
-        )
-    else:
+@ontology.route('/work/<path:title>/')
+def page(title):
+    try:
+        res = session.query(Work).filter(Work.name == title).one()
+    except NoResultFound:
         abort(404)
+    return render_template(
+        'page_work.html',
+        page_title='cliche.io - contents of ' + title,
+        type='Work',
+        title=title,
+        res=res
+    )
