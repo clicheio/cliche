c3dc534510ab1d629b6302fbca1296e6018b5bb5
miaekim <beautiful.mimikim@gmail.com>
2014-09-03 18:06:56 +0900

    API without test

--- sparql/load_dbpedia.py
+++ sparql/load_dbpedia.py
@@ -4,31 +4,115 @@ import urllib.parse
 from SPARQLWrapper import JSON, SPARQLWrapper
 
 
-def load_dbpedia(limit, page):
-    ''' used OFFSET and LIMIT(40,000) for paging query '''
+def select_dbpedia(query):
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
     sparql.setReturnFormat(JSON)
+    sparql.setQuery(query)
+    tuples = sparql.query().convert()['results']['bindings']
+    return[{k: v['value'] for k, v in mydict.items()} for mydict in tuples]
+
+
+def select_property(s='dbpedia-owl:Writer', s_name='property'):
+    prefix = {
+        'owl:': 'http://www.w3.org/2002/07/owl#',
+        'xsd:': 'http://www.w3.org/2001/XMLSchema#',
+        'rdfs:': 'http://www.w3.org/2000/01/rdf-schema#',
+        'rdf:': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+        'foaf:': 'http://xmlns.com/foaf/0.1/',
+        'dc:': 'http://purl.org/dc/elements/1.1/',
+        ':': 'http://dbpedia.org/resource/',
+        'dbpedia2:': 'http://dbpedia.org/property/',
+        'dbpedia:': 'http://dbpedia.org/',
+        'skos:': 'http://www.w3.org/2004/02/skos/core#',
+        'dbpedia-owl:': 'http://dbpedia.org/ontology/',
+        'dbpprop:': 'http://dbpedia.org/property/'
+        }
+
+    query = '''
+    SELECT DISTINCT ?property
+    WHERE {{
+        ?subject a {} .
+        ?subject ?property ?object .
+    }}
+    '''.format(s)
+    p = select_dbpedia(query)
+
+    for x in p:
+        for k, v in prefix.items():
+            x['property'] = x['property'].replace(v, k)
+
+    pts = [tu['property'] for tu in p]
+
+    return pts
+
+
+def select_by_relation(
+    p=['dbpprop:author', 'dbpedia-owl:writer', 'dbpedia-owl:author'],
+        s_name='subject', o_name='object', limit=10):
+    if(len(p) < 1):
+        raise ValueError('at least one porperty required')
+
+    filt = '?p = {}'.format(p[0])
+    for x in p[1:]:
+        filt += '\n            || ?p = {}'.format(x)
     query = '''
         PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
         PREFIX dbpprop: <http://dbpedia.org/property/>
         SELECT DISTINCT
-            ?work
-            (group_concat( STR(?author) ; SEPARATOR="\\n") as ?author)
+            ?{s_name}
+            (group_concat( STR(?{o_name}) ; SEPARATOR="\\n") as ?{o_name})
         WHERE {{
-            ?work ?p ?author
+            ?{s_name} ?p ?{o_name}
         FILTER(
-            (      ?p = dbpprop:author
-                || ?p = dbpedia-owl:author
-                || ?p = dbpedia-owl:writer )
-            && STRSTARTS(STR(?work), "http://dbpedia.org/"))
+            (  {filt}  )
+            && STRSTARTS(STR(?{s_name}), "http://dbpedia.org/"))
         }}
-        GROUP BY ?work
-        LIMIT {}
-        OFFSET {}
-        '''.format(str(limit), str(limit*page))
-    sparql.setQuery(query)
-    tuples = sparql.query().convert()['results']['bindings']
-    return tuples
+        GROUP BY ?{s_name}
+        LIMIT 30
+        OFFSET 30
+        '''.format(s_name=s_name, o_name=o_name, filt=filt)
+    return select_dbpedia(query)
+
+
+def select_by_class(s=['dbpedia-owl:People'],
+                    s_name='subject', entity=['foaf:name'], limit=10):
+    if(len(s) < 1):
+        raise ValueError('at least one class required')
+
+    query = '''
+    PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+    PREFIX dbpprop: <http://dbpedia.org/property/>
+    SELECT DISTINCT
+        ?{}\n'''.format(s_name)
+
+    sel_qry = ''
+    cnd_qry = ''
+
+    for x in entity:
+        if ':' in x:
+            col_name = x.split(':')[1]
+            if '/' in x:
+                col_name = x.split('/')[1]
+        else:
+            col_name = x[:3]
+        sel_qry += '        (group_concat( STR(?{}) ; SEPARATOR="\\n") as ?{})\n'.format(col_name, col_name)
+        cnd_qry += '        ?{} {} ?{} .\n'.format(s_name, x, col_name)
+
+    query += sel_qry
+    query += '''    WHERE {{
+        {{ ?{} a {} . }}'''.format(s_name, s[0])
+    for x in s[1:]:
+        query += '''UNION
+        {{ ?{} a {} . }}'''.format(s_name, x)
+    query += '\n'
+    query += cnd_qry
+    query += '''        }}
+    GROUP BY ?{}
+    '''.format(s_name)
+    if limit is not None:
+        query += 'LIMIT {}'.format(limit)
+    print(query)
+    return select_dbpedia(query)
 
 
 # sqlite3
@@ -52,13 +136,15 @@ def save_db(tuples, table):
 
 
 if __name__ == "__main__":
-    TABLE = {
-        'TABLENAME': 'allworks',
-        'PRIMARY': 'work',
-        'FOREIGN': 'author',
-        'LIMIT': 30000,
-        'COUNT': 8
-    }
+    
+    pass
+    # TABLE = {
+    #     'TABLENAME': 'allworks',
+    #     'PRIMARY': 'work',
+    #     'FOREIGN': 'author',
+    #     'LIMIT': 30000,
+    #     'COUNT': 8
+    # }
 
-    for x in range(0, TABLE['COUNT']):
-        save_db(load_dbpedia(TABLE['LIMIT'], x), TABLE)
+    # for x in range(0, TABLE['COUNT']):
+    #     save_db(load_dbpedia(TABLE['LIMIT'], x), TABLE)
