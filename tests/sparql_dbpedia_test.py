7ef2dcabfdead8538c990e497d206de159845aeb
miaekim <beautiful.mimikim@gmail.com>
2014-09-10 17:41:27 +0900

    monkey patching

--- tests/sparql_dbpedia_test.py
+++ tests/sparql_dbpedia_test.py
@@ -1,21 +1,30 @@
 import json
 
-import pprint
-
 from sparql import load_dbpedia as dbpedia
 
 
 def test_select_property():
     fp = open('select_artist.json')
-    tst = json.load(fp)
+    fakeResult = json.load(fp)
     res = dbpedia.select_property(s='dbpedia-owl:Person', json=True)
-    assert tst != res
+    assert fakeResult != res
     fp.close()
 
 
-def test_select_by_relation():
-    def mockreturn(f):
-        return 'a'
+def test_select_by_relation(monkeypatch):
+    class fakeQuery(object):
+        offset = 0
+
+        def convert(self):
+            fp = open('select_relation.json')
+            offset = fakeQuery.offset
+            fakeResult = (json.load(fp))[offset:offset+100:]
+            fakeQuery.offset += 100
+            fp.close()
+            return {"results": {"bindings": fakeResult}}
+
+    monkeypatch.setattr("SPARQLWrapper.SPARQLWrapper.query", fakeQuery)
+
     res = dbpedia.select_by_relation(
         p=[
             'dbpprop:author',
@@ -24,21 +33,30 @@ def test_select_by_relation():
         ],
         s_name='work',
         o_name='author',
-        limit=10)
-    pprint.pprint(res)
+        limit=101)
+    assert len(res) == 101
 
 
-def test_select_by_class():
-    # fp = open('')
-    # tst = json.load(fp)
+def test_select_by_class(monkeypatch):
+    class fakeQuery(object):
+        offset = 0
+
+        def convert(self):
+            fp = open('select_class.json')
+            offset = fakeQuery.offset
+            fakeResult = (json.load(fp))[offset:offset+100:]
+            fakeQuery.offset += 100
+            fp.close()
+            return {"results": {"bindings": fakeResult}}
+
+    monkeypatch.setattr("SPARQLWrapper.SPARQLWrapper.query", fakeQuery)
+
     res = dbpedia.select_by_class(
         s=['dbpedia-owl:Artist'],
         s_name='artists',
         entity=[
             'foaf:name',
-            'dbpedia-owl:birthDate',
-            'dbpedia-owl:description'
+            'dbpedia-owl:birthDate'
             ],
-        limit=10)
-    print(res)
-    # fp.close()
+        limit=3)
+    assert len(res) == 3
