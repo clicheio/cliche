eee836b1e457af7186b0ad8eb19c1733d72f3ff7
item4_macbook_pro <item4_hun@hotmail.com>
2014-09-12 00:02:14 +0900

    change to click library

--- cliche/cli.py
+++ cliche/cli.py
@@ -2,13 +2,15 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
+import code
 import logging.config
 import os
 import pathlib
 import sys
 
 from alembic.util import CommandError
-from flask.ext.script import Manager
+from click import Path, argument, echo, group, option
+from flask import _request_ctx_stack
 
 from .celery import app as celery_app
 from .config import read_config
@@ -71,26 +73,26 @@ def initialize_app(config=None):
     return flask_app
 
 
-manager = Manager(initialize_app)
-
-manager.add_option('-c', '--config',
-                   dest='config',
-                   help='Configuration file (YAML or Python)')
-
-#: (:class:`collections.abc.Callable`) The CLI entry point.
-main = manager.run
+@group()
+def cli():
+    '''cliche for intergrated command for cliche.io service.'''
+    pass
 
 
-@manager.option('revision', nargs='?', default='head',
-                help='Revision upgrade/downgrade to')
-def upgrade(revision):
+@cli.command()
+@option('--config', '-c', type=Path(exists=True),
+        help='Configuration file (YAML or Python)')
+@argument('revision', default='head') # help='Revision upgrade/downgrade to'
+def upgrade(config, revision):
     """Creates the database tables, or upgrade it to the latest revision."""
+    initialize_app(config)
+
     logging_config = dict(ALEMBIC_LOGGING)
     logging.config.dictConfig(logging_config)
     try:
         engine = get_database_engine()
     except RuntimeError as e:
-        print(e, file=sys.stderr)
+        echo(e, file=sys.stderr)
     else:
         try:
             upgrade_database(engine, revision)
@@ -99,16 +101,87 @@ def upgrade(revision):
                 try:
                     downgrade_database(engine, revision)
                 except CommandError as e:
-                    print(e, file=sys.stderr)
+                    echo(e, file=sys.stderr)
             else:
-                print(e, file=sys.stderr)
+                echo(e, file=sys.stderr)
 
 
-@manager.command
-def crawl():
-    """Crawls TVTropes and saves entities into database."""
-    crawl_tvtropes()
+@cli.command()
+@option('--config', '-c', type=Path(exists=True),
+        help='Configuration file (YAML or Python)')
+def crawl(config):
+    '''Crawles TVTropes and saves entities into database.'''
+    initialize_app(config)
+    crawl_tvtropes(app.config)
 
+
+@cli.command()
+@option('--config', '-c', type=Path(exists=True),
+        help='Configuration file (YAML or Python)')
+@option('--no-ipython', help='Do not use the IPython shell',
+        is_flag=True)
+@option('--no-bpython', help='Do not use the BPython shell',
+        is_flag=True)
+def shell(config, no_ipython, no_bpython):
+    '''Runs a Python shell inside Flask application context.'''
+    initialize_app(config)
+    with app.test_request_context():
+        context = dict(app=_request_ctx_stack.top.app)
+
+        if not no_bpython:
+            # Try BPython
+            try:
+                from bpython import embed
+                embed(locals_=context)
+                return
+            except ImportError:
+                pass
+        if not no_ipython:
+            # Try IPython
+            try:
+                try:
+                    # 0.10.x
+                    from IPython.Shell import IPShellEmbed
+                    ipshell = IPShellEmbed()
+                    ipshell(global_ns=dict(), local_ns=context)
+                except ImportError:
+                    # 0.12+
+                    from IPython import embed
+                    embed(user_ns=context)
+                    return
+            except ImportError:
+                pass
+        # Use basic python shell
+        code.interact(local=context)
+
+
+@cli.command()
+@option('--config', '-c', type=Path(exists=True),
+        help='Configuration file (YAML or Python)')
+@option('--host', '-h')
+@option('--port', '-p', type=int)
+@option('--threaded', is_flag=True)
+@option('--processes', type=int, default=1)
+@option('--passthrough-errors', is_flag=True)
+@option('--debug/--no-debug', '-d/-D', default=False,
+        help='enable the Werkzeug debugger (DO NOT use in production code)')
+@option('--reload/--no-reload', '-r/-R', default=False,
+        help='monitor Python files for changes (not 100% safe for production use)')
+def runserver(config, host, port, threaded, processes, passthrough_errors, debug, reload):
+    '''Runs the Flask development server i.e. app.run()'''
+    initialize_app(config)
+    app.run(host=host,
+            port=port,
+            debug=debug,
+            use_debugger=debug,
+            use_reloader=reload,
+            threaded=threaded,
+            processes=processes,
+            passthrough_errors=passthrough_errors)
+
+
+#: (:class:`collections.abc.Callable`) The CLI entry point.
+main = cli
 
 if __name__ == '__main__':
     main()
