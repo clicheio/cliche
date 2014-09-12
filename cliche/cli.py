594e1b14630e9bf40dd7e390e4d9fa1bc154b2ac
item4_macbook_pro <item4_hun@hotmail.com>
2014-09-12 15:50:07 +0900

    remove manager from __all__

--- cliche/cli.py
+++ cliche/cli.py
@@ -20,7 +20,7 @@ from .web.db import get_database_engine
 
 from .services.tvtropes.crawler import crawl as crawl_tvtropes
 
-__all__ = ('get_database_engine', 'initialize_app', 'main', 'manager')
+__all__ = ('get_database_engine', 'initialize_app', 'main')
 
 
 ALEMBIC_LOGGING = {
