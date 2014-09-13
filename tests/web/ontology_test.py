1ff22d04520adcdede8d943c2b0f8a958ab73df8
item4_macbook_pro <item4_hun@hotmail.com>
2014-09-13 16:45:05 +0900

    change contain_text to assert_contain_text

--- tests/web/ontology_test.py
+++ tests/web/ontology_test.py
@@ -6,7 +6,7 @@ from lxml.html import document_fromstring
 from cliche.work import Award, Genre, Work
 
 
-def contain_text(text, path, data):
+def assert_contain_text(text, path, data):
     def inspect(haystack):
         return haystack is not None and text in haystack
 
@@ -18,10 +18,8 @@ def contain_text(text, path, data):
                 yield inspect(element.text)
 
     tree = document_fromstring(str(data)).xpath(path)
-    if not tree:
-        return False
-
-    return any(traverse(tree))
+    assert tree
+    assert any(traverse(tree))
 
 
 def test_index(fx_session, fx_flask_client):
@@ -32,7 +30,7 @@ def test_index(fx_session, fx_flask_client):
 def test_work_list(fx_session, fx_flask_client):
     # case 1: non-exists document
     rv = fx_flask_client.get('/work/')
-    assert contain_text('No contents here now.', '//tbody/tr/td', rv.data)
+    assert_contain_text('No contents here now.', '//tbody/tr/td', rv.data)
 
     # case 2: add document
     work = Work(name='Story of Your Life')
@@ -41,7 +39,7 @@ def test_work_list(fx_session, fx_flask_client):
         fx_session.add(work)
 
     rv = fx_flask_client.get('/work/')
-    assert contain_text('Story of Your Life', '//tbody/tr/td/a', rv.data)
+    assert_contain_text('Story of Your Life', '//tbody/tr/td/a', rv.data)
 
 
 def test_work_page(fx_session, fx_flask_client):
@@ -56,7 +54,7 @@ def test_work_page(fx_session, fx_flask_client):
         fx_session.add(work)
 
     rv = fx_flask_client.get('/work/Story%20of%20Your%20Life/')
-    assert contain_text('Story of Your Life', '//h1/text()', rv.data)
+    assert_contain_text('Story of Your Life', '//h1/text()', rv.data)
 
     # case 3: set attributes
     work.published_at = datetime.date(2010, 10, 26)
@@ -64,48 +62,48 @@ def test_work_page(fx_session, fx_flask_client):
         fx_session.add(work)
 
     rv = fx_flask_client.get('/work/Story%20of%20Your%20Life/')
-    assert contain_text('2010-10-26', '//tbody/tr/td', rv.data)
+    assert_contain_text('2010-10-26', '//tbody/tr/td', rv.data)
 
     work.number_of_pages = 281
     with fx_session.begin():
         fx_session.add(work)
 
     rv = fx_flask_client.get('/work/Story%20of%20Your%20Life/')
-    assert contain_text('281', '//tbody/tr/td', rv.data)
+    assert_contain_text('281', '//tbody/tr/td', rv.data)
 
     work.isbn = '1931520720'
     with fx_session.begin():
         fx_session.add(work)
 
     rv = fx_flask_client.get('/work/Story%20of%20Your%20Life/')
-    assert contain_text('1931520720', '//tbody/tr/td', rv.data)
+    assert_contain_text('1931520720', '//tbody/tr/td', rv.data)
 
     work.awards.add(Award(name='Nebula Award'))
     with fx_session.begin():
         fx_session.add(work)
 
     rv = fx_flask_client.get('/work/Story%20of%20Your%20Life/')
-    assert contain_text('Nebula Award', '//tbody/tr/td/ul/li/a', rv.data)
+    assert_contain_text('Nebula Award', '//tbody/tr/td/ul/li/a', rv.data)
 
     work.awards.add(Award(name='Sturgeon Award'))
     with fx_session.begin():
         fx_session.add(work)
 
     rv = fx_flask_client.get('/work/Story%20of%20Your%20Life/')
-    assert contain_text('Nebula Award', '//tbody/tr/td/ul/li/a', rv.data)
-    assert contain_text('Sturgeon Award', '//tbody/tr/td/ul/li/a', rv.data)
+    assert_contain_text('Nebula Award', '//tbody/tr/td/ul/li/a', rv.data)
+    assert_contain_text('Sturgeon Award', '//tbody/tr/td/ul/li/a', rv.data)
 
     work.genres.add(Genre(name='Short Stories'))
     with fx_session.begin():
         fx_session.add(work)
 
     rv = fx_flask_client.get('/work/Story%20of%20Your%20Life/')
-    assert contain_text('Short Stories', '//tbody/tr/td/ul/li/a', rv.data)
+    assert_contain_text('Short Stories', '//tbody/tr/td/ul/li/a', rv.data)
 
     work.genres.add(Genre(name='SF'))
     with fx_session.begin():
         fx_session.add(work)
 
     rv = fx_flask_client.get('/work/Story%20of%20Your%20Life/')
-    assert contain_text('Short Stories', '//tbody/tr/td/ul/li/a', rv.data)
-    assert contain_text('SF', '//tbody/tr/td/ul/li/a', rv.data)
+    assert_contain_text('Short Stories', '//tbody/tr/td/ul/li/a', rv.data)
+    assert_contain_text('SF', '//tbody/tr/td/ul/li/a', rv.data)
