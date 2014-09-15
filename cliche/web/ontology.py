40a9557b23f40d49562fce3f2e65550020967b53
item4_macbook_pro <item4_hun@hotmail.com>
2014-09-15 22:52:53 +0900

    support Credit Model for Work.credits relationship

--- cliche/web/ontology.py
+++ cliche/web/ontology.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from flask import Blueprint, abort, render_template
+import itertools
 from sqlalchemy.orm.exc import NoResultFound
 
 from .db import session
-from ..work import Work
+from ..work import Credit, Work
 
 ontology = Blueprint('ontology', __name__,
                      template_folder='templates/ontology/')
@@ -33,8 +34,16 @@ def page(title):
         work = session.query(Work).filter_by(name=title).one()
     except NoResultFound:
         abort(404)
+    credits = session.query(Credit) \
+                     .filter_by(work=work) \
+                     .order_by(Credit.team_id)
+    grouped_credits = [
+        (team_id, list(group))
+        for team_id, group in itertools.groupby(credits, lambda c: c.team_id)
+    ]
     return render_template(
         'page_work.html',
         title=title,
-        work=work
+        work=work,
+        grouped_credits=grouped_credits
     )
