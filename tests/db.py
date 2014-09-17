230ca260b119e91ca2011d6dae00aa22caea8e6c
miaekim <beautiful.mimikim@gmail.com>
2014-09-17 21:48:57 +0900

    app-> flask_app, added celery_app

--- tests/db.py
+++ tests/db.py
@@ -4,8 +4,9 @@ import contextlib
 from sqlalchemy import create_engine
 from sqlalchemy.pool import NullPool, StaticPool
 
+from cliche.celery import app as celery_app
 from cliche.orm import Base, Session
-from cliche.web.app import app
+from cliche.web.app import app as flask_app
 
 __all__ = ('DEFAULT_DATABASE_URL', 'get_database_url', 'get_engine',
            'get_session')
@@ -23,7 +24,7 @@ def get_database_url(url=None):
 
 def get_engine(url=None, echo=False):
     url = get_database_url(url)
-    app.config['DATABASE_URL'] = url
+    flask_app.config['DATABASE_URL'] = url
     connect_args = {}
     options = {'connect_args': connect_args, 'poolclass': NullPool}
     if url == DEFAULT_DATABASE_URL:
@@ -33,7 +34,8 @@ def get_engine(url=None, echo=False):
         connect_args['check_same_thread'] = False
         options['poolclass'] = StaticPool
     engine = create_engine(url, echo=echo, **options)
-    app.config['DATABASE_ENGINE'] = engine
+    flask_app.config['DATABASE_ENGINE'] = engine
+    celery_app.conf['DATABASE_ENGINE'] = engine
     return engine
 
 
