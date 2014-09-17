6a7e6b3459157f711149670c00ce654c1cf7a9f8
miaekim <beautiful.mimikim@gmail.com>
2014-09-17 21:50:58 +0900

    flake8

--- tests/loader_test.py
+++ tests/loader_test.py
@@ -1,6 +1,6 @@
-from sqlalchemy.exc import IntegrityError
 from cliche.services.wikipedia import loader as dbpedia
 from cliche.services.wikipedia.WorkAuthor import WorkAuthor
+
 
 def test_loader(fx_session):
     dbpedia.load_page(1)
