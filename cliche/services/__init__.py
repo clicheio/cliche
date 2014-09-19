3df7ce1bc26dbe0ae31cfd1724084a5f66b77922
Tki April <tki@tki.so>
2014-09-19 17:55:04 +0900

    Add some docs related to adding new services

--- cliche/services/__init__.py
+++ cliche/services/__init__.py
@@ -1,4 +1,31 @@
 """:mod:`cliche.services` --- Interfacing external services
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+How to add a new external service
+---------------------------------
+
+In order to add a new service to cliche, you must create a subpackage under
+:data:`cliche.services` and expose some methods referring to interfaces,
+using :file:`__init__.py`.
+
+Interfaces needed to be exposed
+-------------------------------
+
+- :func:`sync()`: Method to delay a main crawling task to the queue. It should
+  be decorated with :code:`@app.task` to be defined as a celery app worker
+  task.
+
+Example :file:`__init__.py`
+---------------------------
+
+.. sourcecode:: python
+
+   from .crawler import crawl as sync  # noqa
+
+
+   __all__ = 'sync',
+
+Note that you will need the import lines annotated with :code:`# noqa` because
+otherwise :program:`flake8` will consider it as unused import.
+
 """
