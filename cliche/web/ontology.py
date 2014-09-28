87913de3146839b05414dbe01f6516c4d20187a4
Jaewon Song <sjw0687@gmail.com>
2014-09-29 05:57:19 +0900

    adjust frontend and test on it for multiple titles

--- cliche/web/ontology.py
+++ cliche/web/ontology.py
@@ -11,13 +11,27 @@ It widely uses Flask_ as its web framework.
 import itertools
 
 from flask import Blueprint, abort, render_template
+from sqlalchemy import func
+from sqlalchemy.orm import aliased
 from sqlalchemy.orm.exc import NoResultFound
 
 from .db import session
-from ..work import Credit, Work
+from ..work import Credit, Title, Work
 
 ontology = Blueprint('ontology', __name__,
                      template_folder='templates/ontology/')
+
+
+def get_primary_title(work_id):
+    max_reference_count = session.query(func.max(Title.reference_count)) \
+                                 .filter(Title.work_id == work_id) \
+                                 .scalar()
+    title = session.query(Title.title) \
+                   .filter(Title.work_id == work_id) \
+                   .filter(Title.reference_count == max_reference_count) \
+                   .first() \
+                   .title
+    return title
 
 
 @ontology.route('/')
@@ -28,22 +42,30 @@ def index():
 
 @ontology.route('/work/')
 def list_():
-    """A list of works."""
-    names = [
-        name
-        for name, in session.query(Work.name).order_by(Work.name)
-    ]
+    """A list of id-name pairs of works."""
+    w = aliased(Work)
+    t = aliased(Title)
+    res = session.query(w, t) \
+                 .filter(w.id == t.work_id) \
+                 .filter(t.reference_count ==
+                         session.query(func.max(Title.reference_count))
+                                .filter(Title.work_id == t.work_id)
+                                .correlate(t)) \
+                 .order_by(t.title) \
+                 .all()
+    work_list = [(work.id, title.title) for work, title in res]
+
     return render_template(
         'work_list.html',
-        work_list=names
+        work_list=work_list
     )
 
 
-@ontology.route('/work/<path:title>/')
-def page(title):
+@ontology.route('/work/<path:id>/')
+def page(id):
     """More detailed data of a work."""
     try:
-        work = session.query(Work).filter_by(name=title).one()
+        work = session.query(Work).filter_by(id=id).one()
     except NoResultFound:
         abort(404)
     credits = session.query(Credit) \
@@ -53,9 +75,10 @@ def page(title):
         (team_id, list(group))
         for team_id, group in itertools.groupby(credits, lambda c: c.team_id)
     ]
+
     return render_template(
         'page_work.html',
-        title=title,
+        title=get_primary_title(id),
         work=work,
         grouped_credits=grouped_credits
     )
--- tests/web/ontology_test.py
+++ tests/web/ontology_test.py
@@ -3,7 +3,7 @@ import datetime
 from lxml.html import document_fromstring
 
 from cliche.people import Person, Team
-from cliche.work import Credit, Genre, Role, Work
+from cliche.work import Credit, Genre, Role, Title, Work
 
 
 def assert_contain_text(text, expr, data):
@@ -30,7 +30,8 @@ def test_work_list(fx_session, fx_flask_client):
     assert_contain_text('No contents here now.', 'tbody>tr>td', rv.data)
 
     # case 2: add document
-    work = Work(name='Story of Your Life')
+    work = Work()
+    work.titles.update({Title(title='Story of Your Life')})
 
     with fx_session.begin():
         fx_session.add(work)
@@ -41,16 +42,16 @@ def test_work_list(fx_session, fx_flask_client):
 
 def test_work_page(fx_session, fx_flask_client):
     # case 1: non-exists document
-    rv = fx_flask_client.get('/work/Story%20of%20Your%20Life/')
+    rv = fx_flask_client.get('/work/1/')
     assert rv.status_code == 404
 
     # case 2: add document
-    work = Work(name='Story of Your Life')
-
+    work = Work()
+    work.titles.update({Title(title='Story of Your Life')})
     with fx_session.begin():
         fx_session.add(work)
 
-    rv = fx_flask_client.get('/work/Story%20of%20Your%20Life/')
+    rv = fx_flask_client.get('/work/{}/'.format(work.id))
     assert_contain_text('Story of Your Life', 'h1', rv.data)
     assert_contain_text('Story of Your Life', 'tr.name>td', rv.data)
 
@@ -59,21 +60,21 @@ def test_work_page(fx_session, fx_flask_client):
     with fx_session.begin():
         fx_session.add(work)
 
-    rv = fx_flask_client.get('/work/Story%20of%20Your%20Life/')
+    rv = fx_flask_client.get('/work/{}/'.format(work.id))
     assert_contain_text('2010-10-26', 'tr.published_at>td', rv.data)
 
     work.genres.add(Genre(name='Short Stories'))
     with fx_session.begin():
         fx_session.add(work)
 
-    rv = fx_flask_client.get('/work/Story%20of%20Your%20Life/')
+    rv = fx_flask_client.get('/work/{}/'.format(work.id))
     assert_contain_text('Short Stories', 'tr.genres>td', rv.data)
 
     work.genres.add(Genre(name='SF'))
     with fx_session.begin():
         fx_session.add(work)
 
-    rv = fx_flask_client.get('/work/Story%20of%20Your%20Life/')
+    rv = fx_flask_client.get('/work/{}/'.format(work.id))
     assert_contain_text('Short Stories', 'tr.genres>td', rv.data)
     assert_contain_text('SF', 'tr.genres>td', rv.data)
 
@@ -83,7 +84,7 @@ def test_work_page(fx_session, fx_flask_client):
     with fx_session.begin():
         fx_session.add(credit)
 
-    rv = fx_flask_client.get('/work/Story%20of%20Your%20Life/')
+    rv = fx_flask_client.get('/work/{}/'.format(work.id))
     assert_contain_text('Ted Chiang', 'tr.credits>td>table>tbody>tr>td.name',
                         rv.data)
     assert_contain_text(Role.author.value,
@@ -98,7 +99,8 @@ def test_work_page(fx_session, fx_flask_client):
 
 def test_complex_credits(fx_session, fx_flask_client):
     """Data: http://www.animenewsnetwork.com/encyclopedia/anime.php?id=12376"""
-    work = Work(name='Fate/Zero')
+    work = Work()
+    work.titles.update({Title(title='Fate/Zero')})
     author_credit = Credit(person=Person(name='Akihiko Uda'), work=work,
                            role=Role.author)
     ufotable = Team(name='ufotable')
@@ -115,10 +117,11 @@ def test_complex_credits(fx_session, fx_flask_client):
     ]
 
     with fx_session.begin():
+        fx_session.add(work)
         fx_session.add(author_credit)
         fx_session.add_all(artist_credits)
 
-    rv = fx_flask_client.get('/work/Fate%2FZero/')
+    rv = fx_flask_client.get('/work/{}/'.format(work.id))
     assert document_fromstring(rv.data).xpath(
         '//tr/td[@class="name"]/a[text()="Akihiko Uda"]'
         '/../../td[@class="role"]/a[text()="{}"]'.format(Role.author.value)
