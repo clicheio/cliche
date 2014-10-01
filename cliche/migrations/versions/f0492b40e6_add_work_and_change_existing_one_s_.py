14d0527589b25845288eb48bc211d42723252ac4
Miae Kim <miaekim@live.com>
2014-10-01 16:52:01 +0900

    purify migration file

--- cliche/migrations/versions/f0492b40e6_add_work_and_change_existing_one_s_.py
+++ cliche/migrations/versions/f0492b40e6_add_work_and_change_existing_one_s_.py
@@ -7,15 +7,10 @@ Create Date: 2014-10-01 02:01:47.389283
 """
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = 'f0492b40e6'
 down_revision = '223f67eabc3'
-
-credits_role = postgresql.ENUM(
-    'artist', 'author', 'editor', 'director', 'unknown', name='credits_role'
-)
 
 
 def upgrade():
@@ -25,19 +20,7 @@ def upgrade():
         sa.Column('author', sa.String(), nullable=True),
         sa.PrimaryKeyConstraint('work')
     )
-    op.alter_column(
-        'credits',
-        'role',
-        existing_type=credits_role,
-        nullable=True
-    )
 
 
 def downgrade():
-    op.alter_column(
-        'credits',
-        'role',
-        existing_type=credits_role,
-        nullable=True
-    )
     op.drop_table('wikipedia_works')
