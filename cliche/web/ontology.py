0988a9e99eff5abb29d4da1daf73fbc586e93e5b
Kim, Jinsu <item4_hun@hotmail.com>
2014-11-21 03:48:47 +0900

    add trope pages

--- cliche/web/ontology.py
+++ cliche/web/ontology.py
@@ -15,14 +15,14 @@ from flask import Blueprint, abort, render_template
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 
 from .db import session
-from ..work import Credit, Work
+from ..work import Credit, Trope, Work
 
 ontology = Blueprint('ontology', __name__,
                      template_folder='templates/ontology/')
 
 
 @ontology.route('/work/')
-def list_():
+def work_list():
     """A list of id-name pairs of works."""
     res = session.query(Work.id,
                         Work.canonical_name(Locale.parse('en_US'))
@@ -38,7 +38,7 @@ def list_():
 
 
 @ontology.route('/work/<path:title>/')
-def page(title):
+def work_page(title):
     """More detailed data of a work."""
     try:
         stmt = session.query(
@@ -63,11 +63,51 @@ def page(title):
         (team_id, list(group))
         for team_id, group in itertools.groupby(credits, lambda c: c.team_id)
     ]
+    trope_names = sorted(x.name for x in work.tropes)
 
     return render_template(
         'page_work.html',
         title=work.canonical_name(Locale.parse('en_US')),
         locale=Locale.parse('en_US'),
         work=work,
-        grouped_credits=grouped_credits
+        grouped_credits=grouped_credits,
+        trope_names=trope_names
+    )
+
+
+@ontology.route('/trope/')
+def trope_list():
+    """A list of name of tropes."""
+    res = session.query(Trope.name) \
+        .order_by(Trope.name) \
+        .all()
+    trope_list = [name for name, in res]
+
+    return render_template(
+        'trope_list.html',
+        trope_list=trope_list
+    )
+
+
+@ontology.route('/trope/<path:name>/')
+def trope_page(name):
+    """More detailed data of a work."""
+    try:
+        trope = session.query(Trope).filter_by(name=name).one()
+    except NoResultFound:
+        abort(404)
+    except MultipleResultsFound:
+        # When there are works with the same canonical name,
+        # the ambiguity should be solved in a appropriate way.
+        abort(404)
+
+    work_names = sorted(
+        x.canonical_name(Locale.parse('en_US')) for x in trope.works
+    )
+
+    return render_template(
+        'page_trope.html',
+        name=trope.name,
+        trope=trope,
+        work_names=work_names
     )
