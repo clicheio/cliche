b39ec1e23f1052f2bf8345dd5e750a3fb9a5df9d
Tki April <tki@tki.so>
2015-01-17 04:38:41 +0900

    Removed code for deleting single trope

--- cliche/cli.py
+++ cliche/cli.py
@@ -13,7 +13,6 @@ from alembic.util import CommandError
 from click import Path, argument, echo, group, option
 from flask import _request_ctx_stack
 from sassutils.wsgi import SassMiddleware
-from sqlalchemy.sql.functions import count
 from werkzeug.utils import import_string
 
 from .celery import app as celery_app
@@ -24,7 +23,7 @@ from .sqltypes import HashableLocale
 from .web.app import (app as flask_app,
                       setup_sentry as flask_setup_sentry)
 from .web.db import get_database_engine, session
-from .work import Trope, Work, WorkTrope
+from .work import Trope, Work
 from .services.align import alignment
 
 
@@ -5339,15 +5338,6 @@ def dummy():
         Base.metadata.create_all(bind=engine)
         with session.begin():
             session.add_all(works)
-
-        single_tropes = session.query(WorkTrope.trope_id). \
-            group_by(WorkTrope.trope_id). \
-            having(count() == 1).all()
-
-        for trope in single_tropes:
-            trope = session.query(Trope).get(trope)
-            with session.begin():
-                session.delete(trope)
 
 #: (:class:`collections.abc.Callable`) The CLI entry point.
 main = cli
