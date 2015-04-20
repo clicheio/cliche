eac75f6769064e960e9d85d90407f28a8e749932
Kim, Jinsu <item4_hun@hotmail.com>
2015-04-20 18:36:13 +0900

    upgrade almost deps

--- setup.py
+++ setup.py
@@ -33,29 +33,29 @@ def readme():
 install_requires = {
     # Entity classes
     'SQLAlchemy >= 1.0.0',
-    'alembic >= 0.7.0',
+    'alembic >= 0.7.5',
     # Configuration
-    'PyYAML >= 3.10',
+    'PyYAML >= 3.11',
     # Crawler
-    'celery >= 3.1.13',
-    'lxml >= 3.3.5',
-    'psycopg2 >= 2.5.3',  # FIXME
-    'requests >= 2.3.0',
+    'celery >= 3.1.17',
+    'lxml >= 3.4.3',
+    'psycopg2 >= 2.6',  # FIXME
+    'requests >= 2.6.0',
     # Web
-    'Flask >= 0.10',
-    'Werkzeug >= 0.9',
+    'Flask >= 0.10.1',
+    'Werkzeug >= 0.10.4',
     # Sparql
     'SPARQLWrapper >= 1.6.2',
     # CLI
-    'click >= 3.3',
+    'click >= 4.0',
     # Locale
     'Babel >= 1.3',
     # SCSS
-    'libsass >= 0.6.0',
+    'libsass >= 0.7.0',
     # OAuth client
-    'Flask-OAuthlib >= 0.7.0',
+    'Flask-OAuthlib >= 0.9.1',
     # Sentry: Log Aggregation
-    'raven >= 5.1.1',
+    'raven >= 5.2.0',
     'blinker >= 1.3',
 }
 
@@ -66,16 +66,16 @@ if sys.version_info < (3, 4, 0):
     }
 
 tests_require = {
-    'pytest >= 2.5.0',
+    'pytest >= 2.6.4',
     'cssselect >= 0.9.1',
 }
 
 docs_require = {
-    'Sphinx >= 1.2',
+    'Sphinx >= 1.2.3',
 }
 
 deploy_requires = {
-    "invoke == 0.9.0",
+    "invoke == 0.9.0",  # FIXME: least version is 0.10.1. check it with deploy
 }
 
 
